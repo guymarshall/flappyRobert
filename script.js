b08915cc7d3b9ffa2d6dce24bf4016e5c0c9cdda
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-11-16 12:20:27 +0000

    Add barebones jump function with check in gravity to see if not jumping

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let block = document.getElementById("block");
 let hole = document.getElementById("hole");
 let character = document.getElementById("character");
+let jumping = 0;
 
 hole.addEventListener("animationiteration", () => {
     let random = -((Math.random() * 300) + 150);
@@ -8,5 +9,17 @@ hole.addEventListener("animationiteration", () => {
 });
 setInterval(() => {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    character.style.top = (characterTop + 3) + "px";
+    if (jumping == 0) {
+        character.style.top = (characterTop + 3) + "px";
+    }
 }, 10);
+
+let jump = () => {
+    jumping = 1;
+    let jumpCount = 0;
+    let jumpInterval = setInterval(() => {
+        let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        character.style.top = (characterTop - 5) + "px";
+        jumpCount++;
+    }, 10);
+};
