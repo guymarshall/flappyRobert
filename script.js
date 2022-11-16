ea1ec5fae2fe63b942291844d92e7ac92072332e
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-11-16 12:15:42 +0000

    Add setInterval to simulate gravity

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let block = document.getElementById("block");
 let hole = document.getElementById("hole");
+let character = document.getElementById("character");
 
 hole.addEventListener("animationiteration", () => {
     let random = -((Math.random() * 300) + 150);
     hole.style.top = random + "px";
 });
+setInterval(() => {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    character.style.top = (characterTop + 3) + "px";
+}, 10);
