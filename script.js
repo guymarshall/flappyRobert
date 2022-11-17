d9ff8ad019769257ced768cd75793d41eeb6078e
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-11-17 12:23:55 +0000

    Add game over for touching bottom or pipe

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ let block = document.getElementById("block");
 let hole = document.getElementById("hole");
 let character = document.getElementById("character");
 let jumping = 0;
+let counter = 0;
 
 hole.addEventListener("animationiteration", () => {
     let random = -((Math.random() * 300) + 150);
     hole.style.top = random + "px";
+    counter++;
 });
 setInterval(() => {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if (jumping == 0) {
         character.style.top = (characterTop + 3) + "px";
+    }
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    let holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    let cTop = -(500 - characterTop);
+    if ((characterTop > 480) || ((blockLeft < 20) && (blockLeft > - 50) && ((cTop < holeTop) || (cTop > holeTop + 130)))) {
+        alert(`Game Over. Score: ${counter}`);
+        character.style.top = 100 + "px";
+        counter = 0;
     }
 }, 10);
 
